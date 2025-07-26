3f26f3acbe25b4dd74107d18bf68ad54425f40dd
Optimistab <45667909+optimistab@users.noreply.github.com>
2025-07-26 22:34:46 +0530

    added database creds

--- db.ts
+++ db.ts
@@ -1,11 +1,21 @@
+// import { Pool } from "pg";
+
+// const pool = new Pool({
+//   user: "postgres",           // your Postgres username
+//   host: "localhost",          // or your DB host
+//   database: "inventoryprodb",  // your DB name
+//   password: "User%40123",  // your DB password
+//   port: 5432,                 // default Postgres port
+// });
+
+// export default pool;
+
 import { Pool } from "pg";
 
 const pool = new Pool({
-  user: "postgres",           // your Postgres username
-  host: "localhost",          // or your DB host
-  database: "inventoryprodb",  // your DB name
-  password: "User%40123",  // your DB password
-  port: 5432,                 // default Postgres port
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }, // Important for Render's managed PG
 });
 
 export default pool;
+
